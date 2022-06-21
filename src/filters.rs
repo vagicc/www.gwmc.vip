adf1585c51ae19d84efb6dc09dc9bd9f7dec6073
临来笑笑生 <luck@elapse.date>
2022-06-21 21:08:54 +0800

    首页

---
+++ src/filters.rs
@@ -0,0 +1,19 @@
+use warp::Filter;
+
+use crate::routes::home_route;
+
+pub fn all_routes() -> impl warp::Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone
+{
+    let favicon = warp::get()
+        .and(warp::path("favicon.ico"))
+        .and(warp::path::end())
+        .and(warp::fs::file("./static/favicon.ico"));
+
+    let dir = warp::path("static").and(warp::fs::dir("./static"));
+    let home = home_route::index();
+
+    // let demo = demo_route::all();
+
+    let routes = home.or(dir).or(favicon);
+    routes
+}
---
+++ src/main.rs
@@ -0,0 +1,43 @@
+use crate::common::get_env;
+use std::net::SocketAddr;
+
+mod common;
+mod db;
+mod filters;
+mod format_logger;
+mod handlers;
+mod routes;
+mod template;
+
+#[tokio::main]
+async fn main() {
+    println!("跟我买车:www.gwmc.vip!");
+
+    // env_logger::init();
+    let log_level = crate::format_logger::get_log_level();
+    // 自定义日志输出格式
+    env_logger::Builder::new()
+        .format(crate::format_logger::formatlog)
+        .filter(None, log_level)
+        .init();
+
+    log::info!("such information");
+    log::warn!("o_O");
+    log::error!("much error");
+    log::debug!("高度");
+
+    let routes = filters::all_routes();
+
+    //取得https证书等
+    let cert_path = get_env("cert_path");
+    let key_path = get_env("key_path");
+    let ip_addr = get_env("ip_address");
+    let socket_addr: SocketAddr = ip_addr.as_str().parse().unwrap();
+
+    warp::serve(routes)
+        .tls()
+        .cert_path(cert_path)
+        .key_path(key_path)
+        .run(socket_addr)
+        .await;
+}
