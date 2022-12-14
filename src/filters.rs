1d424c2e03b276ae31335da571a6a601980f8849
临来笑笑生 <luck@elapse.date>
2022-12-14 11:27:09 +0800

    申请https证书

--- src/filters.rs
+++ src/filters.rs
@@ -10,11 +10,13 @@ pub fn all_routes() -> impl warp::Filter<Extract = impl warp::Reply, Error = war
         .and(warp::path::end())
         .and(warp::fs::file("./static/favicon.ico"));
 
+    //.well-known
+    let well = warp::path(".well-known").and(warp::fs::dir("./static"));
     let dir = warp::path("static").and(warp::fs::dir("./static"));
     let home = home_route::index();
     let lawsuit_autocar = lawsuit_autocar_route::list();
     // let demo = demo_route::all();
 
-    let routes = home.or(dir).or(favicon).or(lawsuit_autocar);
+    let routes = home.or(well).or(dir).or(favicon).or(lawsuit_autocar);
     routes
 }
