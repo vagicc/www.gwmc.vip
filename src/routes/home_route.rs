9cfcb420fd60a9bb95e1b6102c06f7b33066c53c
临来笑笑生 <luck@elapse.date>
2023-03-08 21:26:12 +0800

    屏蔽首页

--- src/routes/home_route.rs
+++ src/routes/home_route.rs
@@ -3,13 +3,13 @@ use warp::Filter;
 
 /* 访问站点 / 时的路由 */
 pub fn index() -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
-    let home = warp::get()
-        .and(warp::path::end())
-        .and_then(home_handler::index);
-    home
-
-    // use crate::handlers::lawsuit_autocar_handler;
-    // warp::get()
+    // let home = warp::get()
     //     .and(warp::path::end())
-    //     .and_then(|| async { lawsuit_autocar_handler::list(1).await })
+    //     .and_then(home_handler::index);
+    // home
+
+    use crate::handlers::lawsuit_autocar_handler;
+    warp::get()
+        .and(warp::path::end())
+        .and_then(|| async { lawsuit_autocar_handler::list(1).await })
 }
