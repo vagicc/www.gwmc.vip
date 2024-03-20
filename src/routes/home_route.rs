cd49e02736d7168adc25422ebc66d88a9249f773
临来笑笑生 <luck@elapse.date>
2024-03-20 16:03:40 +0800

    区分线上和开发时首页（用于网站未完成）

--- src/routes/home_route.rs
+++ src/routes/home_route.rs
@@ -3,13 +3,15 @@ use warp::Filter;
 
 /* 访问站点 / 时的路由 */
 pub fn index() -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
-    let home = warp::get()
-        .and(warp::path::end())
-        .and_then(home_handler::index);
-    home
-
-    // use crate::handlers::lawsuit_autocar_handler;
-    // warp::get()
+    //开发时首页，暂时
+    // let home = warp::get()
     //     .and(warp::path::end())
-    //     .and_then(|| async { lawsuit_autocar_handler::list(1).await })
+    //     .and_then(home_handler::index);
+    // home
+
+    //外网没做好前，首页直接跳转到列表页
+    use crate::handlers::lawsuit_autocar_handler;
+    warp::get()
+        .and(warp::path::end())
+        .and_then(|| async { lawsuit_autocar_handler::list(1).await })
 }
